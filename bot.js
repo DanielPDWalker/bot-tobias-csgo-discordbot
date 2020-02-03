a1cc23f57533f43551a7e8f6afef97d113990533
danielpdwalker <danielpdwalker@gmail.com>
2020-02-03 13:36:18 +0000

    Changed some of the bots cleanup functionality for better user inferface, Changed some post functionaility as well, again for user benefit. Set scraping timer to repeat every hour after bot launch.

--- bot.js
+++ bot.js
@@ -21,15 +21,30 @@ var LAST_REQUEST = new Date().getTime();
 var COOLDOWN_MESSAGE;
 
 // String to search posts for
-var SEARCH_STRING = ls.STRING_TO_SEARCH_FOR
+var SEARCH_STRING = ls.STRING_TO_SEARCH_FOR;
 
 // User to search for through reddit api
-var TARGET_USER = ls.REDDIT_TARGET
+var TARGET_USER = ls.REDDIT_TARGET_USER;
+var TARGET_SUBREDDIT = ls.REDDIT_TARGET_SUBREDDIT;
 
+var CSGO_NEWEST_PATCHNOTES_CACHED;
 var CSGO_NEWEST_PATCHNOTES;
 var CSGO_NEWEST_TIME = 0;
 
-function redditScrape(msg) {
+var CHANNEL_TO_USE;
+
+
+function scrapeConsoleOutput(s_post_list, s_scraped_post_count) {
+  let dt = new Date(CSGO_NEWEST_PATCHNOTES.created * 1000);
+  console.log("-------------------------");
+  console.log("total posts found matching string: " + s_post_list.length);
+  console.log("total posts scraped from reddit: " + s_scraped_post_count);
+  console.log("Date & APPROX TIME Posted for newest patchnotes: " + dt.toString());
+  console.log("-------------------------");
+};
+
+
+function redditScrape(target_channel, startup = 0) {
   let newestPostTime = 0;
   let scraped_post_count = 0;
   r.getUser(TARGET_USER).getSubmissions({limit: 200}).then(posts => {
@@ -37,15 +52,15 @@ function redditScrape(msg) {
     for (var i = 0; i < posts.length; i++) {
       scraped_post_count += 1;
       if (posts[i].title.startsWith(SEARCH_STRING)) {
-        if (posts[i].subreddit_name_prefixed == 'r/GlobalOffensive') {
+        if (posts[i].subreddit_name_prefixed == TARGET_SUBREDDIT) {
           post_list.push(posts[i]);
         }else {
           //pass
         }
       }
     }
-    if (post_list.lenth === 0) {
-      msg.channel.send("No CSGO patch notes found! Something went wrong?");
+    if (post_list.length === 0) {
+      target_channel.send("No CSGO patch notes found! Something went wrong?");
       return;
     }
     for (var i = 0; i < post_list.length; i++) {
@@ -54,50 +69,62 @@ function redditScrape(msg) {
         newestPostTime = post_list[i].created;
       }
     }
-    if (msg != 0){
-      msg.channel.send(CSGO_NEWEST_PATCHNOTES.url);
+    if (typeof CSGO_NEWEST_PATCHNOTES_CACHED === 'undefined') {
+      CSGO_NEWEST_PATCHNOTES_CACHED = CSGO_NEWEST_PATCHNOTES;
     }
-    let dt = new Date(CSGO_NEWEST_PATCHNOTES.created * 1000);
-    console.log("-------------------------");
-    console.log("total posts found matching string: " + post_list.length);
-    console.log("total posts scraped from reddit: " + scraped_post_count);
-    console.log("Date & APPROX TIME Posted for newest patchnotes: " + dt.toString());
-    console.log("-------------------------");
-
+    if (startup === 1) {
+      scrapeConsoleOutput(post_list, scraped_post_count);
+      LAST_REQUEST = new Date().getTime();
+    }else {
+      CSGO_NEWEST_PATCHNOTES_CACHED = CSGO_NEWEST_PATCHNOTES;
+      target_channel.send(CSGO_NEWEST_PATCHNOTES.title + '\n' + CSGO_NEWEST_PATCHNOTES.url);
+      scrapeConsoleOutput(post_list, scraped_post_count);
+      LAST_REQUEST = new Date().getTime();
+    }
   })
-}
+};
 
 
-function cleanUpOldCooldownMessages(msg) {
+function cleanUpOldCooldownMessages() {
   if (typeof COOLDOWN_MESSAGE != 'undefined') {
     COOLDOWN_MESSAGE.delete();
   }
-}
+};
 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  redditScrape(0);
+  CHANNEL_TO_USE = client.channels.find(ch => ch.name === ls.CHANNEL_TO_USE);
+  redditScrape(CHANNEL_TO_USE, 1);
 });
+
 
 client.on('message', msg => {
   if (msg.content === '!csgopatch'){
-      msg.channel.send(CSGO_NEWEST_PATCHNOTES.url);
+      CHANNEL_TO_USE.send(CSGO_NEWEST_PATCHNOTES.title + '\n' + CSGO_NEWEST_PATCHNOTES.url);
+
   }
   if (msg.content === '!csgopatchget'){
     var now = new Date().getTime();
     if (now < LAST_REQUEST + COOLDOWN) {
       var time_diff = Math.round(((LAST_REQUEST + COOLDOWN) - now) / 1000);
-      msg.channel.send(time_diff +  " seconds timeout remaining.");
-      cleanUpOldCooldownMessages(msg);
-      COOLDOWN_MESSAGE = msg;
+      cleanUpOldCooldownMessages();
+      CHANNEL_TO_USE.send(time_diff +  " seconds timeout remaining.").then((sent_msg) =>{
+        COOLDOWN_MESSAGE = sent_msg;
+      });
     } else {
-      redditScrape(msg);
+      redditScrape(CHANNEL_TO_USE);
     }
+  }
+  if (msg.content === '!csgohelp'){
+      CHANNEL_TO_USE.send('Commands for BOT Tobias are: !csgopatch (returns most recent patch post) and !csgopatchget (try to send a request to get updated patchnotes).');
   }
 });
 
+
 client.login(ls.TOKEN);
+client.guild;
+setInterval(function() { redditScrape(CHANNEL_TO_USE); }, 60*60*1000);
 
 
 
